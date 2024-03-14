062c8f57311790a9db5ae63faed0b23fe2190c0e
Xianyi Liang <liangxianyi2011@gmail.com>
2024-03-14 17:03:53 -0400

    setup auth

---
+++ src/auth.ts
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth";
+import GitHub from "@auth/core/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./db";
+
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+// const AUTH_SECRET = process.env.AUTH_SECRET;
+
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET)
+  throw new Error("GitHub OAuth credentials are not set up.");
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHub({
+      clientId: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    // current bug fix
+    async session({ session, user }: any) {
+      if (session && user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+});
